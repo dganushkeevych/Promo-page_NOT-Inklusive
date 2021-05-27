54adfc8883637d92e9e0594db953871a036c584f
dganushkeevych <dganushkevych@gmail.com>
2021-05-27 23:13:09 +0300

    Final changes

--- js/main.js
+++ js/main.js
@@ -21,13 +21,13 @@ let nCount = selector => {
   };
   
   let a = 0;
-  function goo() {
+  $(window).scroll(function () {
     // The .offset() method allows us to retrieve the current position of an element  relative to the document
     let oTop = $(".numbers").offset().top - window.innerHeight;
     if (a == 0 && $(window).scrollTop() >= oTop) {
       a++;
       nCount(".rect > h1");
     }
-  };
+  });
   
   
